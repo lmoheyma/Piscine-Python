f544a1f75bc5121350117badb5787984d3240dbe
lmoheyma <louismoheymani@gmail.com>
2024-06-02 17:49:49 +0200

    ✅ Day00 -> ex00

---
+++ Day00/ex00/Hello.py
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Hello.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/02 17:42:00 by lmoheyma          #+#    #+#              #
+#    Updated: 2024/06/02 17:49:06 by lmoheyma         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "France!")
+ft_set = {"Hello", "Paris!"}
+ft_dict = {"Hello" : "42Paris!"}
+
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
