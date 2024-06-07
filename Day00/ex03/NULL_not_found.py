55028f7969a366ab1a1cdd86f29824c1e89d4e7a
lmoheyma <louismoheymani@gmail.com>
2024-06-07 22:50:00 +0200

    ✅ ex03 without builtins

--- Day00/ex03/NULL_not_found.py
+++ Day00/ex03/NULL_not_found.py
@@ -6,25 +6,24 @@
 #    By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/07 15:01:51 by lmoheyma          #+#    #+#              #
-#    Updated: 2024/06/07 15:26:18 by lmoheyma         ###   ########.fr        #
+#    Updated: 2024/06/07 22:48:49 by lmoheyma         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import builtins
 from math import isnan
 
 def NULL_not_found(object : any) -> int:
 	object_type = type(object)
-	
+
 	if object is None:
 		print(f"Nothing : {object} {object_type}")
-	elif object_type is builtins.float and isnan(object):
+	elif isinstance(object, float) and object != object:
 		print(f"Cheese : {object} {object_type}")
-	elif object_type is builtins.int and object == 0:
+	elif isinstance(object, int) and not isinstance(object, bool) and object == 0:
 		print(f"Zero : {object} {object_type}")
-	elif object_type is builtins.str and object == "":
+	elif isinstance(object, str) and object == "":
 		print(f"Empty : {object_type}")
-	elif object_type is builtins.bool and object == False:
+	elif isinstance(object, bool) and object == False:
 		print(f"Fake : {object} {object_type}")
 	else:
 		print("Type not found")
