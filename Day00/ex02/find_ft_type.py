ed39813ef00b514b66fd2638dad463974c2c2451
lmoheyma <louismoheymani@gmail.com>
2024-06-07 22:35:48 +0200

    ✅ ex02 without builtins

--- Day00/ex02/find_ft_type.py
+++ Day00/ex02/find_ft_type.py
@@ -6,26 +6,22 @@
 #    By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/06 12:58:07 by lmoheyma          #+#    #+#              #
-#    Updated: 2024/06/07 15:06:23 by lmoheyma         ###   ########.fr        #
+#    Updated: 2024/06/07 22:34:17 by lmoheyma         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import builtins
-
 def all_thing_is_obj(object: any) -> int:
     object_type = type(object)
-    match object_type:
-        case builtins.list:
-            print(f"List : {object_type}")
-        case builtins.tuple:
-            print(f"Tuple : {object_type}")
-        case builtins.set:
-            print(f"Set : {object_type}")
-        case builtins.dict:
-            print(f"Dict : {object_type}")
-        case builtins.str:
-            print(f"{object} is in the kitchen : {object_type}")
-        case _:
+    if isinstance(object, list):
+        print(f"List : {object_type}")
+    elif isinstance(object, tuple):
+        print(f"Tuple : {object_type}")
+    elif isinstance(object, set):
+        print(f"Set : {object_type}")
+    elif isinstance(object, dict):
+        print(f"Dict : {object_type}")
+    elif isinstance(object, str):
+        print(f"{object} is in the kitchen : {object_type}")
+    else:
             print("Type not found")
     return 42
-        
