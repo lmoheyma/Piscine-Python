2daf30e7488027d5e68bc35d8fe6e2afc626390a
lmoheyma <louismoheymani@gmail.com>
2024-06-08 21:13:05 +0200

    ✅ ex08

---
+++ Day00/ex08/Loading.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Loading.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/08 16:36:52 by lmoheyma          #+#    #+#              #
+#    Updated: 2024/06/08 21:12:37 by lmoheyma         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_tqdm(lst: range):
+	count = len(lst)
+	def show(j):
+		x = int(85 * j / count)
+		print(f"{(j/count) * 100:.0f}%|{u'█'*x}{('.'*(85 - x))}| {j}/{count}", end='\r', flush=True)
+	show(0.1)
+	for i, item in enumerate(lst):
+		yield item
+		show(i + 1)
