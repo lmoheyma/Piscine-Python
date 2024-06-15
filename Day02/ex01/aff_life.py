ccb3164fbc63c960df24f1d4aae1b3d7759e506c
lmoheyma <louismoheymani@gmail.com>
2024-06-15 05:37:24 +0200

    ✅ ex01

--- Day02/ex01/aff_life.py
+++ Day02/ex01/aff_life.py
@@ -2,21 +2,26 @@ from matplotlib import pyplot as plt
 from load_csv import load
 
 
-def aff_life(campus: str, path: str) -> None:
-	df = load(path)
-	# print(df)
-	temp = df[df['country'] == 'France']
-	xValues = temp.columns[1:]
-	print(xValues)
-	xValues = xValues.astype(str)
-	yValues = temp.iloc[0, 1:].values
-	df.plot(xValues, yValues, label='france', title='France life expectancy Projections')
-	plt.show()
+def aff_life(path: str) -> None:
+    df = load(path)
+    temp = df[df['country'] == 'France']
+    temp = temp.set_index('country').T
+    temp.reset_index(inplace=True)
+    temp.columns = ['Year', 'Life Expectancy']
+    temp['Year'] = temp['Year'].astype(int)
+    temp['Life Expectancy'] = temp['Life Expectancy'].astype(float)
+    plt.plot(temp['Year'], temp['Life Expectancy'])
+    plt.title('France Life expectancy Projections')
+    plt.xlabel('Year')
+    plt.xticks(temp['Year'][::40], rotation=45)
+    plt.ylabel('Life Expectancy')
+    plt.tight_layout()
+    plt.show()
 
 def main():
-	aff_life("Paris", "life_expectancy_years.csv")
-	return 0
+    aff_life("life_expectancy_years.csv")
+    return 0
 
 
 if __name__ == "__main__":
-	main()
+    main()
